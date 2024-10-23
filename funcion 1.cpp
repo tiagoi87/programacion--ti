cbb4d9c7cd92c87af3d7b880ad6ae90aef0b53d9
tiagoi87 <tiago2appac@gmail.com>
2024-10-23 12:36:00 -0300

    Add files via upload
    son dos funciones que una convierte de metros a centimetros y la otra de centimetros a metros.

---
+++ funcion 1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//funcion para convertir metros a centimetros
+int mts_a_cmts(int metros) {
+	return metros * 100;
+}
+
+//funcion para convertir centimetros a metros
+int cmts_a_mts(int centimetros) {
+	return centimetros / 100;
+	
+}
+
+int main(){
+	return 0;
+}
